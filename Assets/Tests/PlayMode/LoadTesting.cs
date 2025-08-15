dc5a5087a62702214c3062cfa227b6fa03fa0e82
Stormra1d <lennschlott@gmail.com>
2025-08-15 08:58:26 +0200

    fix: re-add Performance Tests

--- Assets/Tests/PlayMode/LoadTesting.cs
+++ Assets/Tests/PlayMode/LoadTesting.cs
@@ -1,161 +1,161 @@
-//using System.Collections;
-//using NUnit.Framework;
-//using UnityEngine;
-//using UnityEngine.TestTools;
-//using Unity.PerformanceTesting;
-//using UnityEngine.SceneManagement;
-//using System.Collections.Generic;
-//using System.Linq;
-//using UnityEngine.Profiling;
-//using System.Diagnostics;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Unity.PerformanceTesting;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Profiling;
+using System.Diagnostics;
 
-//public class LoadTesting
-//{
-//    private GameObject playerGameObject;
-//    private readonly string playerPrefabPath = "Player";
-//    private readonly string enemyPrefabPath = "EnemyAI";
-//    private List<GameObject> spawnedEnemies = new();
+public class LoadTesting
+{
+    private GameObject playerGameObject;
+    private readonly string playerPrefabPath = "Player";
+    private readonly string enemyPrefabPath = "EnemyAI";
+    private List<GameObject> spawnedEnemies = new();
 
-//    [UnitySetUp]
-//    public IEnumerator Setup()
-//    {
-//        SceneManager.LoadScene("PerformanceTestScene");
-//        yield return null;
+    [UnitySetUp]
+    public IEnumerator Setup()
+    {
+        SceneManager.LoadScene("PerformanceTestScene");
+        yield return null;
 
-//        var playerPrefab = Resources.Load<GameObject>(playerPrefabPath);
-//        Assert.IsNotNull(playerPrefab, "Player prefab should be available");
-//        playerGameObject = GameObject.Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        var playerPrefab = Resources.Load<GameObject>(playerPrefabPath);
+        Assert.IsNotNull(playerPrefab, "Player prefab should be available");
+        playerGameObject = GameObject.Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
 
-//        spawnedEnemies.Clear();
-//        yield return null;
-//    }
+        spawnedEnemies.Clear();
+        yield return null;
+    }
 
-//    [UnityTearDown]
-//    public IEnumerator Teardown()
-//    {
-//        if (playerGameObject) GameObject.DestroyImmediate(playerGameObject);
-//        foreach (var enemy in spawnedEnemies)
-//        {
-//            if (enemy)
-//            {
-//                GameObject.DestroyImmediate(enemy);
-//            }
-//        }
-//        spawnedEnemies.Clear();
-//        yield return null;
-//    }
+    [UnityTearDown]
+    public IEnumerator Teardown()
+    {
+        if (playerGameObject) GameObject.DestroyImmediate(playerGameObject);
+        foreach (var enemy in spawnedEnemies)
+        {
+            if (enemy)
+            {
+                GameObject.DestroyImmediate(enemy);
+            }
+        }
+        spawnedEnemies.Clear();
+        yield return null;
+    }
 
-//    [UnityTest, Performance]
-//    public IEnumerator SteadyStateTest()
-//    {
-//        var enemyPrefab = Resources.Load<GameObject>(enemyPrefabPath);
-//        Assert.IsNotNull(enemyPrefab, "Enemy prefab should be available");
+    [UnityTest, Performance]
+    public IEnumerator SteadyStateTest()
+    {
+        var enemyPrefab = Resources.Load<GameObject>(enemyPrefabPath);
+        Assert.IsNotNull(enemyPrefab, "Enemy prefab should be available");
 
-//        for (int i = 0; i < 100; i++)
-//        {
-//            Vector3 pos = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
-//            var enemy = GameObject.Instantiate(enemyPrefab, pos, Quaternion.identity);
-//            spawnedEnemies.Add(enemy);
-//        }
+        for (int i = 0; i < 100; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+            var enemy = GameObject.Instantiate(enemyPrefab, pos, Quaternion.identity);
+            spawnedEnemies.Add(enemy);
+        }
 
-//        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
 
-//        for (int i = 0; i < 120; i++)
-//        {
-//            yield return null;
-//            Measure.Custom("SteadyState_FPS", 1.0f / Time.deltaTime);
-//            Measure.Custom("SteadState_MemoryMB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
-//        }
+        for (int i = 0; i < 120; i++)
+        {
+            yield return null;
+            Measure.Custom("SteadyState_FPS", 1.0f / Time.deltaTime);
+            Measure.Custom("SteadState_MemoryMB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
+        }
 
-//        yield return null;
-//    }
+        yield return null;
+    }
 
-//    [UnityTest, Performance]
-//    public IEnumerator RampUpTest()
-//    {
-//        var enemyPrefab = Resources.Load<GameObject>(enemyPrefabPath);
-//        Assert.IsNotNull(enemyPrefab, "Enemy prefab should be available");
-//        int currentCount = 0;
+    [UnityTest, Performance]
+    public IEnumerator RampUpTest()
+    {
+        var enemyPrefab = Resources.Load<GameObject>(enemyPrefabPath);
+        Assert.IsNotNull(enemyPrefab, "Enemy prefab should be available");
+        int currentCount = 0;
 
-//        for (int i = 0; i < 5; i++)
-//        {
-//            for (int j = 0; j < 10; j++)
-//            {
-//                Vector3 pos = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
-//                var enemy = GameObject.Instantiate(enemyPrefab, pos, Quaternion.identity);
-//                spawnedEnemies.Add(enemy);
-//                currentCount++;
-//            }
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Vector3 pos = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+                var enemy = GameObject.Instantiate(enemyPrefab, pos, Quaternion.identity);
+                spawnedEnemies.Add(enemy);
+                currentCount++;
+            }
 
-//            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(2f);
 
-//            for (int f = 0; f < 30; f++)
-//            {
-//                yield return null;
-//                Measure.Custom($"{currentCount}_Enemies_FPS", 1.0f / Time.deltaTime);
-//                Measure.Custom($"{currentCount}_Enemies_MemoryMB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
-//            }
-//        }
-//    }
+            for (int f = 0; f < 30; f++)
+            {
+                yield return null;
+                Measure.Custom($"{currentCount}_Enemies_FPS", 1.0f / Time.deltaTime);
+                Measure.Custom($"{currentCount}_Enemies_MemoryMB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
+            }
+        }
+    }
 
-//    [UnityTest, Performance]
-//    public IEnumerator ConcurrentEnemyChaseTest()
-//    {
-//        var enemyPrefab = Resources.Load<GameObject>(enemyPrefabPath);
-//        Assert.IsNotNull(enemyPrefab, "Enemy prefab should be available");
+    [UnityTest, Performance]
+    public IEnumerator ConcurrentEnemyChaseTest()
+    {
+        var enemyPrefab = Resources.Load<GameObject>(enemyPrefabPath);
+        Assert.IsNotNull(enemyPrefab, "Enemy prefab should be available");
 
-//        for (int i = 0; i < 50; i++)
-//        {
-//            Vector3 pos = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
-//            var enemy = GameObject.Instantiate(enemyPrefab, pos, Quaternion.identity);
-//            spawnedEnemies.Add(enemy);
-//        }
+        for (int i = 0; i < 50; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+            var enemy = GameObject.Instantiate(enemyPrefab, pos, Quaternion.identity);
+            spawnedEnemies.Add(enemy);
+        }
 
-//        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1f);
 
-//        foreach (var enemy in spawnedEnemies)
-//        {
-//            var ai = enemy.GetComponent<EnemyAI>();
-//            if (ai != null)
-//            {
-//                ai.OnPlayerShot(playerGameObject.transform.position);
-//            }
-//        }
+        foreach (var enemy in spawnedEnemies)
+        {
+            var ai = enemy.GetComponent<EnemyAI>();
+            if (ai != null)
+            {
+                ai.OnPlayerShot(playerGameObject.transform.position);
+            }
+        }
 
-//        yield return null;
+        yield return null;
 
-//        for (int i = 0; i < 10; i++)
-//        {
-//            var stopwatch = Stopwatch.StartNew();
-            
-//            yield return null;
+        for (int i = 0; i < 10; i++)
+        {
+            var stopwatch = Stopwatch.StartNew();
 
-//            stopwatch.Stop();
+            yield return null;
 
-//            double cpuTimeMs = stopwatch.Elapsed.TotalMilliseconds;
-//            Measure.Custom("ConcurrentChase_FPS", 1.0f / Time.deltaTime);
-//            Measure.Custom("ConcurrentChase_CPUTimeMS", cpuTimeMs);
-//            Measure.Custom($"ConcurrentChase_MemoryMB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
-//        }
-//    }
+            stopwatch.Stop();
 
-//    [UnityTest, Performance]
-//    public IEnumerator MainGameSceneLoadTime()
-//    {
-//        string sceneName = "Game";
+            double cpuTimeMs = stopwatch.Elapsed.TotalMilliseconds;
+            Measure.Custom("ConcurrentChase_FPS", 1.0f / Time.deltaTime);
+            Measure.Custom("ConcurrentChase_CPUTimeMS", cpuTimeMs);
+            Measure.Custom($"ConcurrentChase_MemoryMB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
+        }
+    }
 
-//        float startTime = Time.realtimeSinceStartup;
-//        var asyncOp = SceneManager.LoadSceneAsync(sceneName);
-//        while (!asyncOp.isDone)
-//        {
-//            yield return null;
-//        }
-//        float loadTime = Time.realtimeSinceStartup - startTime;
-//        Measure.Custom("SceneLoadTime_Sec", loadTime);
-//        UnityEngine.Debug.Log($"Scene: {sceneName} loaded in {loadTime:F2} secpnds");
+    [UnityTest, Performance]
+    public IEnumerator MainGameSceneLoadTime()
+    {
+        string sceneName = "Game";
 
-//        Assert.Less(loadTime, 3.0f, "Scene shouldn't be higher than 3 seconds");
+        float startTime = Time.realtimeSinceStartup;
+        var asyncOp = SceneManager.LoadSceneAsync(sceneName);
+        while (!asyncOp.isDone)
+        {
+            yield return null;
+        }
+        float loadTime = Time.realtimeSinceStartup - startTime;
+        Measure.Custom("SceneLoadTime_Sec", loadTime);
+        UnityEngine.Debug.Log($"Scene: {sceneName} loaded in {loadTime:F2} secpnds");
 
-//        yield return null;
-//    }
-//}
+        Assert.Less(loadTime, 3.0f, "Scene shouldn't be higher than 3 seconds");
+
+        yield return null;
+    }
+}
--- Assets/Tests/PlayMode/StressTests.cs
+++ Assets/Tests/PlayMode/StressTests.cs
@@ -1,189 +1,189 @@
-//using NUnit.Framework;
-//using System.Collections;
-//using System.Collections.Generic;
-//using Unity.PerformanceTesting;
-//using UnityEngine;
-//using UnityEngine.Profiling;
-//using UnityEngine.SceneManagement;
-//using UnityEngine.TestTools;
-//using UnityEngine.UIElements;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PerformanceTesting;
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UIElements;
 
-//public class StressTests
-//{
-//    private List<GameObject> spawnedEnemies = new();
-//    private List<GameObject> spawnedProjectiles = new();
-//    private List<GameObject> spawnedCollectibles = new();
-//    private GameObject playerGameObject;
+public class StressTests
+{
+    private List<GameObject> spawnedEnemies = new();
+    private List<GameObject> spawnedProjectiles = new();
+    private List<GameObject> spawnedCollectibles = new();
+    private GameObject playerGameObject;
 
-//    private List<string> errorLogs = new();
+    private List<string> errorLogs = new();
 
-//    [UnitySetUp]
-//    public IEnumerator Setup()
-//    {
-//        SceneManager.LoadScene("PerformanceTestScene");
-//        yield return null;
+    [UnitySetUp]
+    public IEnumerator Setup()
+    {
+        SceneManager.LoadScene("PerformanceTestScene");
+        yield return null;
 
-//        errorLogs.Clear();
-//        Application.logMessageReceived += OnLogMessageReceived;
+        errorLogs.Clear();
+        Application.logMessageReceived += OnLogMessageReceived;
 
-//        var playerPrefab = Resources.Load<GameObject>("Player");
-//        Assert.IsNotNull(playerPrefab);
-//        playerGameObject = Object.Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        var playerPrefab = Resources.Load<GameObject>("Player");
+        Assert.IsNotNull(playerPrefab);
+        playerGameObject = Object.Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
 
-//        spawnedEnemies.Clear();
-//        spawnedProjectiles.Clear();
-//        spawnedCollectibles.Clear();
+        spawnedEnemies.Clear();
+        spawnedProjectiles.Clear();
+        spawnedCollectibles.Clear();
 
-//        yield return null;
-//    }
+        yield return null;
+    }
 
-//    [UnityTearDown]
-//    public IEnumerator Teardown()
-//    {
-//        Application.logMessageReceived -= OnLogMessageReceived;
+    [UnityTearDown]
+    public IEnumerator Teardown()
+    {
+        Application.logMessageReceived -= OnLogMessageReceived;
 
-//        if (playerGameObject) Object.DestroyImmediate(playerGameObject);
-//        foreach (var gameObject in spawnedEnemies) if (gameObject) Object.DestroyImmediate (gameObject);
-//        foreach (var gameObject in spawnedProjectiles) if (gameObject) Object.DestroyImmediate(gameObject);
-//        foreach (var gameObject in spawnedProjectiles) if (gameObject) Object.DestroyImmediate(gameObject);
+        if (playerGameObject) Object.DestroyImmediate(playerGameObject);
+        foreach (var gameObject in spawnedEnemies) if (gameObject) Object.DestroyImmediate(gameObject);
+        foreach (var gameObject in spawnedProjectiles) if (gameObject) Object.DestroyImmediate(gameObject);
+        foreach (var gameObject in spawnedProjectiles) if (gameObject) Object.DestroyImmediate(gameObject);
 
-//        spawnedEnemies.Clear();
-//        spawnedProjectiles.Clear();
-//        spawnedCollectibles.Clear();
+        spawnedEnemies.Clear();
+        spawnedProjectiles.Clear();
+        spawnedCollectibles.Clear();
 
-//        yield return null;
-//    }
+        yield return null;
+    }
 
-//    void OnLogMessageReceived(string condition, string stackTrace, LogType type)
-//    {
-//        if (type == LogType.Error || type == LogType.Exception)
-//        {
-//            errorLogs.Add($"{type}: {condition}\n{stackTrace}");
-//        }
-//    }
+    void OnLogMessageReceived(string condition, string stackTrace, LogType type)
+    {
+        if (type == LogType.Error || type == LogType.Exception)
+        {
+            errorLogs.Add($"{type}: {condition}\n{stackTrace}");
+        }
+    }
 
-//    [UnityTest, Performance]
-//    public IEnumerator SystemStressTest()
-//    {
-//        int numEnemies = 2000;
-//        int numProjectiles = 5000;
-//        int numCollectibles = 500;
+    [UnityTest, Performance]
+    public IEnumerator SystemStressTest()
+    {
+        int numEnemies = 2000;
+        int numProjectiles = 5000;
+        int numCollectibles = 500;
 
-//        var enemyPrefab = Resources.Load<GameObject>("EnemyAI");
-//        for (int i = 0; i < numEnemies; i++)
-//        {
-//            Vector3 pos = new Vector3(Random.Range(-20, 20), 0, Random.Range(-20, 20));
-//            var gameObject = Object.Instantiate(enemyPrefab, pos, Quaternion.identity);
-//            spawnedEnemies.Add(gameObject);
-//        }
+        var enemyPrefab = Resources.Load<GameObject>("EnemyAI");
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+            var gameObject = Object.Instantiate(enemyPrefab, pos, Quaternion.identity);
+            spawnedEnemies.Add(gameObject);
+        }
 
-//        var bulletPrefab = Resources.Load<GameObject>("Bullet");
-//        for (int i = 0; i < numProjectiles; i++)
-//        {
-//            Vector3 pos = new Vector3(Random.Range(-10, 10), 1f, Random.Range(-10, 10));
-//            var gameObject = Object.Instantiate(bulletPrefab, pos, Quaternion.identity);
-//            var rb = gameObject.GetComponent<Rigidbody>();
-//            if (rb != null)
-//                rb.linearVelocity = Random.onUnitSphere * 30f;
-//            spawnedProjectiles.Add(gameObject);
-//        }
+        var bulletPrefab = Resources.Load<GameObject>("Bullet");
+        for (int i = 0; i < numProjectiles; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-10, 10), 1f, Random.Range(-10, 10));
+            var gameObject = Object.Instantiate(bulletPrefab, pos, Quaternion.identity);
+            var rb = gameObject.GetComponent<Rigidbody>();
+            if (rb != null)
+                rb.linearVelocity = Random.onUnitSphere * 30f;
+            spawnedProjectiles.Add(gameObject);
+        }
 
-//        var collectiblePrefab = Resources.Load<GameObject>("RedRuby");
-//        for (int i = 0; i < numCollectibles; i++)
-//        {
-//            Vector3 pos = new Vector3(Random.Range(-20, 20), 0.5f, Random.Range(-20, 20));
-//            var gameObject = Object.Instantiate(collectiblePrefab, pos, Quaternion.identity);
-//            spawnedCollectibles.Add(gameObject);
-//        }
+        var collectiblePrefab = Resources.Load<GameObject>("RedRuby");
+        for (int i = 0; i < numCollectibles; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-20, 20), 0.5f, Random.Range(-20, 20));
+            var gameObject = Object.Instantiate(collectiblePrefab, pos, Quaternion.identity);
+            spawnedCollectibles.Add(gameObject);
+        }
 
-//        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(5f);
 
-//        for (int i = 0; i < 60; i++)
-//        {
-//            yield return null;
-//            Measure.Custom("SystemStress_FPS", 1.0 / Time.deltaTime);
-//            Measure.Custom("SystemStress_MemoryDB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
-//        }
+        for (int i = 0; i < 60; i++)
+        {
+            yield return null;
+            Measure.Custom("SystemStress_FPS", 1.0 / Time.deltaTime);
+            Measure.Custom("SystemStress_MemoryDB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
+        }
 
-//        Debug.Log($"System Stress Test: {errorLogs.Count} errors/exceptions captured");
-//        foreach (var error in errorLogs) Debug.LogError(error);
+        Debug.Log($"System Stress Test: {errorLogs.Count} errors/exceptions captured");
+        foreach (var error in errorLogs) Debug.LogError(error);
 
-//        yield return null;
-//    }
+        yield return null;
+    }
 
-//    /// <summary>
-//    /// Seems kinda lame. Just the same as above but because those components can't really exist isolated, what is the point?
-//    /// </summary>
-//    /// <returns></returns>
-//    [UnityTest, Performance]
-//    public IEnumerator ComponentStressTest_Projectiles()
-//    {
-//        int numProjectiles = 50000;
+    /// <summary>
+    /// Seems kinda lame. Just the same as above but because those components can't really exist isolated, what is the point?
+    /// </summary>
+    /// <returns></returns>
+    [UnityTest, Performance]
+    public IEnumerator ComponentStressTest_Projectiles()
+    {
+        int numProjectiles = 50000;
 
-//        spawnedProjectiles.Clear();
+        spawnedProjectiles.Clear();
 
-//        var bulletPrefab = Resources.Load<GameObject>("Bullet");
-//        Assert.IsNotNull(bulletPrefab, "Bullet Prefab should be available");
+        var bulletPrefab = Resources.Load<GameObject>("Bullet");
+        Assert.IsNotNull(bulletPrefab, "Bullet Prefab should be available");
 
-//        for (int i = 0; i< numProjectiles; i++)
-//        {
-//            Vector3 pos = new Vector3(Random.Range(-10, 10), 1f, Random.Range(-10, 10));
-//            var gameObject = Object.Instantiate(bulletPrefab, pos, Quaternion.identity);
-//            var rb = gameObject.GetComponent<Rigidbody>();
-//            if (rb != null)
-//                rb.linearVelocity = Random.onUnitSphere * 30f;
-//            spawnedProjectiles.Add(gameObject);
-//        }
-//        yield return new WaitForSeconds(2f);
+        for (int i = 0; i < numProjectiles; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-10, 10), 1f, Random.Range(-10, 10));
+            var gameObject = Object.Instantiate(bulletPrefab, pos, Quaternion.identity);
+            var rb = gameObject.GetComponent<Rigidbody>();
+            if (rb != null)
+                rb.linearVelocity = Random.onUnitSphere * 30f;
+            spawnedProjectiles.Add(gameObject);
+        }
+        yield return new WaitForSeconds(2f);
 
-//        for (int i = 0; i  < 60; i++)
-//        {
-//            yield return null;
-//            Measure.Custom("ComponentStress_Projectile_FPS", 1.0f / Time.deltaTime);
-//            Measure.Custom("ComponentStress_Projectile_MemoryMB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
-//        }
+        for (int i = 0; i < 60; i++)
+        {
+            yield return null;
+            Measure.Custom("ComponentStress_Projectile_FPS", 1.0f / Time.deltaTime);
+            Measure.Custom("ComponentStress_Projectile_MemoryMB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
+        }
 
-//        Debug.Log($"Component Stress Projectiles: {errorLogs.Count} errors / exceptions captured");
-//        foreach (var error in errorLogs) Debug.LogError(error);
-//    }
+        Debug.Log($"Component Stress Projectiles: {errorLogs.Count} errors / exceptions captured");
+        foreach (var error in errorLogs) Debug.LogError(error);
+    }
 
-//    [UnityTest, Performance]
-//    public IEnumerator IsolationStressTest_UI()
-//    {
-//        long memBefore = Profiler.GetTotalAllocatedMemoryLong();
-//        errorLogs.Clear();
-//        playerGameObject.AddComponent<Health>();
+    [UnityTest, Performance]
+    public IEnumerator IsolationStressTest_UI()
+    {
+        long memBefore = Profiler.GetTotalAllocatedMemoryLong();
+        errorLogs.Clear();
+        playerGameObject.AddComponent<Health>();
 
-//        ItemManager itemManager =  playerGameObject.GetComponent<ItemManager>();
-//        GameObject weaponPrefab = Resources.Load<GameObject>("Rifle");
-//        GameObject weaponObject = UnityEngine.Object.Instantiate(weaponPrefab);
-//        Weapon newWeapon = weaponObject.GetComponent<Weapon>();
-//        var healthUI = UnityEngine.Object.FindFirstObjectByType<PlayerHealthUI>();
-//        var ammoUI = UnityEngine.Object.FindFirstObjectByType<AmmoUIManager>();
+        ItemManager itemManager = playerGameObject.GetComponent<ItemManager>();
+        GameObject weaponPrefab = Resources.Load<GameObject>("Rifle");
+        GameObject weaponObject = UnityEngine.Object.Instantiate(weaponPrefab);
+        Weapon newWeapon = weaponObject.GetComponent<Weapon>();
+        var healthUI = UnityEngine.Object.FindFirstObjectByType<PlayerHealthUI>();
+        var ammoUI = UnityEngine.Object.FindFirstObjectByType<AmmoUIManager>();
 
-//        itemManager.ammoUIManager = ammoUI;
-//        itemManager.EquipWeapon(newWeapon);
+        itemManager.ammoUIManager = ammoUI;
+        itemManager.EquipWeapon(newWeapon);
 
-//        Assert.IsNotNull(healthUI);
-//        Assert.IsNotNull(ammoUI);
+        Assert.IsNotNull(healthUI);
+        Assert.IsNotNull(ammoUI);
 
-//        for (int i = 0; i < 10000; i++)
-//        {
-//            healthUI.Update();
-//            ammoUI.UpdateAmmoUI();
-//        }
+        for (int i = 0; i < 10000; i++)
+        {
+            healthUI.Update();
+            ammoUI.UpdateAmmoUI();
+        }
 
-//        yield return null;
+        yield return null;
 
-//        //TODO does this make sense
-//        float allowedThreshold = 2 * 1024 * 1024;
-//        long memAfter = Profiler.GetTotalAllocatedMemoryLong();
-//        Assert.LessOrEqual(memAfter, memBefore + allowedThreshold, "Memory leak detected");
+        //TODO does this make sense
+        float allowedThreshold = 2 * 1024 * 1024;
+        long memAfter = Profiler.GetTotalAllocatedMemoryLong();
+        Assert.LessOrEqual(memAfter, memBefore + allowedThreshold, "Memory leak detected");
 
-//        Measure.Custom("IsolationStress_UI_MemoryMB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
-//        Debug.Log($"Isolation Stress UI: {errorLogs.Count} errors/exceptions captured");
-//        foreach (var error in errorLogs) Debug.LogError(error);
+        Measure.Custom("IsolationStress_UI_MemoryMB", Profiler.GetTotalAllocatedMemoryLong() / (1024.0f * 1024.0f));
+        Debug.Log($"Isolation Stress UI: {errorLogs.Count} errors/exceptions captured");
+        foreach (var error in errorLogs) Debug.LogError(error);
 
-//        yield return null;
-//    }
-//}
+        yield return null;
+    }
+}
